835967bc40f19f08c93def98e2b39ef2da932ee9
honey4251996 <peacehoney425@gmail.com>
2020-05-23 18:18:15 +0300

    updated

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ if (fs.existsSync(dir)) {
     fs.truncate(dir + fileName, 0, function () {
         console.log('PageSpeed.txt content is removed')
         // Run
-        // const testUrl = 'https://css-tricks.com';
-        const testUrl = 'https://www.forrent.com/';
+        const testUrl = 'https://css-tricks.com';
         WebPageTest.run(testUrl, dir, fileName);
     })
 }
